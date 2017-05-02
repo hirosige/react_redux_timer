660b14de68578526164fb40b1fea65d7aa4f46f4
Hiroshige.Negishi <hirosige1@gmail.com>
2017-05-02 23:50:40 +0700

    initial commit

---
+++ js/components/Timer.js
@@ -0,0 +1,3 @@
+/**
+ * Created by hiro on 2017/05/02.
+ */
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/**
+ * Created by hiro on 2017/05/02.
+ */
+
+module.exports = {
+    entry: './js./main.js',
+    output: {
+        path: 'dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
